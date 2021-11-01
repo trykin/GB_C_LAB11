a3b14010892cc7a6a64cb08958779b78468db800
Peter Novikov <peter@p-home.ru>
2021-11-01 09:32:55 +0300

    Добавьте файлы проекта.

---
+++ GB_C_LAB12/GB_C_LAB12.cpp
@@ -0,0 +1,23 @@
+﻿// Без проверки на ошибки и модульности.
+// Здесь вроде в библиотеки выносить нечего
+// И как это делается я вроде в курсовом показал
+// Маке там же
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#include <stdio.h>
+#include <io.h>
+int main(int argc, char* argv[])
+{
+    FILE* in, * out;
+    int  n;
+    char buf[512];
+    in = fopen(argv[1], "rb");
+    out = fopen(argv[2], "w");
+    while ((n = read(fileno(in), buf, sizeof buf)) > 0)
+    {
+        write(fileno(out), buf, n);
+    }
+    return 0;
+}
